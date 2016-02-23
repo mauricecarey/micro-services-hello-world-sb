201ec900d80d4e0b9d71648de9c16fd9082d37c4
Maurice Carey <maurice@mauricecarey.com>
2016-02-23 13:15:48 -0700

    Initial checkin of code generated by https://start.spring.io using Spring Initializr https://github.com/spring-io/initializr/ Includes: Lombok, Session, Redis, JPA, PostgreSQL, Web, and Actuator.

---
+++ src/main/java/com/prometheussoftwarellc/hello/MicroserviceHelloWorldApplication.java
@@ -0,0 +1,12 @@
+package com.prometheussoftwarellc.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceHelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceHelloWorldApplication.class, args);
+	}
+}
