e37e42c5f7f8df00023ba67143018cd1e05fcdc0
Maurice Carey <maurice@mauricecarey.com>
2016-02-24 12:02:31 -0700

    Reformatting whitespace to 2 spaces instead of tabs.

--- src/main/java/com/prometheussoftwarellc/hello/MicroserviceHelloWorldApplication.java
+++ src/main/java/com/prometheussoftwarellc/hello/MicroserviceHelloWorldApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MicroserviceHelloWorldApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MicroserviceHelloWorldApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(MicroserviceHelloWorldApplication.class, args);
+  }
 }
